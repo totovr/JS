856d92e1925719dffdfbbbcdadb21d4dfee64eb8
Antonio <a.vegaramirez76@gmail.com>
2019-02-05 14:13:57 +0900

    Serial read using node

---
+++ Node/helloWorld/index.js
@@ -0,0 +1,3 @@
+// counting “node” as element 0 and “index.js” as element 1
+var name = process.argv[2]
+console.log("Hello, and welcome to node, " + name)
---
+++ Node/nodeSerialExample/index.js
@@ -0,0 +1,32 @@
+var SerialPort = require('serialport');// include the library
+
+// get port name from the command line:
+var portName = process.argv[2];
+
+// Setup the port
+var myPort = new SerialPort(portName, {baudRate:115200});
+
+var Readline = SerialPort.parsers.Readline; // make instance of Readline parser
+var parser = new Readline(); // make a new parser to read ASCII lines
+myPort.pipe(parser); // pipe the serial stream to the parser
+
+myPort.on('open', showPortOpen);
+parser.on('data', readSerialData);
+myPort.on('close', showPortClose);
+myPort.on('error', showError);
+
+function showPortOpen() {
+   console.log('port open. Data rate: ' + myPort.baudRate);
+}
+ 
+function readSerialData(data) {
+   console.log(data);
+}
+ 
+function showPortClose() {
+   console.log('port closed.');
+}
+ 
+function showError(error) {
+   console.log('Serial port error: ' + error);
+}
