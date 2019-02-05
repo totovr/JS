af6509fa743bc88079e8d74076e5910863506c52
Antonio <a.vegaramirez76@gmail.com>
2019-02-05 20:22:15 +0900

    map analog read value

--- Node/SocketBroadcast/public/sketch.js
+++ Node/SocketBroadcast/public/sketch.js
@@ -1,14 +1,17 @@
 var text;     // variable for the text div you'll create
 var socket = new WebSocket("ws://localhost:8081");
 
+let font,
+    fontsize = 40;
+
 function setup() {
     // The socket connection needs two event listeners:
     socket.onopen = openSocket;
     socket.onmessage = showData;
 
     // make a new div and position it at 10, 10:
-    text = createDiv("Sensor reading:");
-    text.position(10, 10);
+    text = createDiv("Sensor reading: ");
+    text.position(20, 20);
 }
 
 function openSocket() {
@@ -18,7 +21,8 @@ function openSocket() {
 
 function showData(result) {
     // when the server returns, show the result in the div:
-    text.html("Sensor reading: " + result.data);
-    xPos = int(result.data);        // convert result to an integer
-    text.position(xPos, 10);        // position the text
+    var value = map(result.data, 0, 4095, 0, 1023);
+    text.html("Sensor reading: " + value);
+    xPos = int(value);        // convert result to an integer
+    text.position(xPos, 20);        // position the text
 }
